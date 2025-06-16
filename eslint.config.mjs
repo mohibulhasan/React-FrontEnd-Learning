41aa18f1ce402f810e592d66e35b51c201f7d261
mohibulhasan <mohibul-hasan@outlook.com>
2025-06-16 11:22:32 +0100

    eslint react plugin installed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,29 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 
 export default [
   js.configs.recommended,
   {
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
+
+  {
     files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
+      rules: {
+        "react/no-unescaped-entities": "off",
+        "react/prop-types": "off",
+      },
       globals: { ...globals.browser, ...globals.node },
       parserOptions: {
         ecmaFeatures: {
