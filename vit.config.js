afcd80d9c450ae9cb9569359714d9daf3e1bca43
mohibulhasan <mohibul-hasan@outlook.com>
2025-06-16 08:04:55 +0100

    Installed React

---
+++ vit.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// This is a Vite configuration file for a React project.
+export default defineConfig({
+  plugins: [react()],
+});
